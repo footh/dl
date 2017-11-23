f29bc02c5771c51af63b62b8a5f79b5d70ab7855
jfaath <jeff@ibotta.com>
2017-11-22 21:32:12 -0500

    zone 1,3

--- pscreening/config.py
+++ pscreening/config.py
@@ -11,8 +11,8 @@ PSCREENING_LOCAL_HOME = os.getenv('PSCREENING_LOCAL_HOME', '.')
 SUBMISSION_MODELS = [
     'zone5-vgg16-d200-c1-e2-bs32-lr001-20171121-052129-02-0.102.h5',
     'zone17-vgg16-d200-c1-e3-bs32-lr001-20171121-053939-03-0.012.h5',
-    'zone1-vgg16-d200-c1-e6-bs32-lr001-20171121-150637-06-0.394.h5',
-    'zone3-vgg16-d200-c1-e6-bs32-lr001-20171121-155008-04-0.324.h5',
+    'zone1-vgg16-d200-c1-e6-bs32-lr001-SUBWGHTS.h5',
+    'zone3-vgg16-d200-c1-e10-bs32-lr001-SUBWGHTS.h5',
     'zone6-vgg16-d200-c1-e6-bs32-lr001-20171121-161816-04-0.125.h5',
     'zone7-vgg16-d200-c1-e6-bs32-lr001-20171121-164023-06-0.264.h5',
     'zone8-vgg16-d200-c1-e6-bs32-lr001-20171121-172658-06-0.142.h5',
