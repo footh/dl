14688f7da9385d8e2bf5c593fefd230e5a0572ff
jfaath <jeff@ibotta.com>
2017-12-02 17:56:24 -0700

    fix

--- pscreening/trainer.py
+++ pscreening/trainer.py
@@ -5,13 +5,13 @@ import os
 import config
 import setup_data as sd
 
-def train(zones, epochs=10, batch_size=32, learning_rate=0.001,
+def train(zones, epochs=10, batch_size=20, learning_rate=0.001,
           version=None, gpus=4, mtype='vgg16', starting_model_file=None,
-          img_dim=224, channels=1, subtract_mean=False):
+          img_dim=224, channels=1):
     
     file_prefix = pscreening.train(zones, epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
                                    version=version, gpus=gpus, mtype=mtype, starting_model_file=starting_model_file,
-                                   img_dim=img_dim, channels=channels, subtract_mean=subtract_mean)
+                                   img_dim=img_dim, channels=channels)
     
     print(f"Training completed. File prefix: {file_prefix}. Running tests...")
     
@@ -23,7 +23,7 @@ def train(zones, epochs=10, batch_size=32, learning_rate=0.001,
 
         model_file = os.path.basename(model_file)
         val_loss = float(sd.get_file_name(model_file).split('-')[-1])
-        results = pscreening.testm(model_file, subtract_mean=subtract_mean)
+        results = pscreening.testm(model_file)
         results_dict[model_file] = [val_loss] + results
         
     print(f"Testing completed. Printing results...")
